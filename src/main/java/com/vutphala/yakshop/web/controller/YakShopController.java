6f9570c0ca7094691e6acc5ee93e94c0917be8df
vutphala <srinath@vutphala.com>
2014-03-14 15:41:28 +0530

    first commit

--- src/main/java/com/vutphala/yakshop/web/controller/YakShopController.java
+++ src/main/java/com/vutphala/yakshop/web/controller/YakShopController.java
@@ -125,20 +125,50 @@ public class YakShopController {
 	@RequestMapping(value = "/order/{day}", method = RequestMethod.POST)
 	@ResponseBody
 	public String checkOrder(@PathVariable Integer day,
-			OrderRequest orderRequest, HttpServletResponse response) {
+			@RequestBody OrderRequest orderRequest, HttpServletResponse response) {
 
 		HeardInfo info = heardService.getHeardDetailsForTheDay(day);
 		JSONArray yaks = new JSONArray();
 		try {
-			JSONObject stock = new JSONObject();
-			stock.put("milk", info.getMilkStock());
-			stock.put("skins", info.getWoolStock());
-
-			response.setStatus(HttpServletResponse.SC_OK);
-			response.setContentType("");
-			response.getOutputStream().write(stock.toString().getBytes());
-			response.getOutputStream().flush();
-
+			
+			boolean milkAvailable=false;
+			boolean skinAvailable=false;
+			if(orderRequest!=null && orderRequest.getOrder()!=null){
+				if(orderRequest.getOrder().getMilk()<=info.getMilkStock()){
+					milkAvailable=true;
+				}
+				if(orderRequest.getOrder().getSkins()<=info.getWoolStock()){
+					skinAvailable=true;
+				}
+			}
+			if(!milkAvailable && !skinAvailable){
+				response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+				response.setContentType("");
+				response.getOutputStream().write(" STOCK NOT AVAILABLE".getBytes());
+				response.getOutputStream().flush();
+			}else if(milkAvailable && !skinAvailable){
+				response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
+				response.setContentType("application/json");
+				JSONObject stock = new JSONObject();
+				stock.put("milk", orderRequest.getOrder().getMilk());
+				response.getOutputStream().write(stock.toString().getBytes());
+				response.getOutputStream().flush();
+			}else if(!milkAvailable && skinAvailable){
+				response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
+				response.setContentType("application/json");
+				JSONObject stock = new JSONObject();
+				stock.put("skins", orderRequest.getOrder().getSkins());
+				response.getOutputStream().write(stock.toString().getBytes());
+				response.getOutputStream().flush();
+			}else{
+				response.setStatus(HttpServletResponse.SC_CREATED);
+				response.setContentType("application/json");
+				JSONObject stock = new JSONObject();
+				stock.put("milk", orderRequest.getOrder().getMilk());
+				stock.put("skins", orderRequest.getOrder().getSkins());
+				response.getOutputStream().write(stock.toString().getBytes());
+				response.getOutputStream().flush();
+			}
 		} catch (IOException e) {
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 			// TODO Auto-generated catch block
@@ -158,20 +188,21 @@ public class YakShopController {
 
 	@RequestMapping(value = "/stock/calendar", method = RequestMethod.GET)
 	@ResponseBody
-	public String stockCalendar(@RequestParam("start") String paramStartDate,@RequestParam("end") String paramEndDateValue, HttpServletResponse response) {
+	public String stockCalendar(@RequestParam("start") String paramStartDate,
+			@RequestParam("end") String paramEndDateValue,
+			HttpServletResponse response) {
 
 		Calendar startDate = Calendar.getInstance();
 		Calendar endDate = Calendar.getInstance();
 		try {
-			startDate.setTimeInMillis(Long.parseLong(paramStartDate)*1000);
-			endDate.setTimeInMillis(Long.parseLong(paramEndDateValue)*1000);
+			startDate.setTimeInMillis(Long.parseLong(paramStartDate) * 1000);
+			endDate.setTimeInMillis(Long.parseLong(paramEndDateValue) * 1000);
 		} catch (Exception e) {
 			// will be sending only today data
 		}
 
-		int startDays = Days.daysBetween(
-				new DateTime(startDate.getTimeInMillis()),
-				new DateTime(new Date())).getDays();
+		int startDays = Days.daysBetween(new DateTime(new Date()),
+				new DateTime(startDate.getTimeInMillis())).getDays();
 		int endDays = Days.daysBetween(new DateTime(new Date()),
 				new DateTime(endDate.getTimeInMillis())).getDays();
 		int noOfDays = 0;
@@ -179,7 +210,22 @@ public class YakShopController {
 
 		try {
 			JSONArray events = new JSONArray();
-			if (startDays > 0 && endDays > 0) {
+			if (startDays <=0 && endDays > 0) {
+				
+				for (int calCounter = 0; calCounter <= endDays; calCounter++) {
+					HeardInfo info = heardService
+							.getHeardDetailsForTheDay(calCounter);
+					JSONObject evendata = new JSONObject();
+
+					evendata.put("title", "Milk :" + info.getMilkStock()
+							+ "  Skin : " + info.getWoolStock());
+					evendata.put("start", (fmt.print(new DateTime(new Date())
+							.plusDays(calCounter))));
+					events.put(evendata);
+				}
+
+				
+			} else if (startDays > 0 && endDays > 0) {
 
 				// noOfDays=noOfDays+startDays;
 				for (int calCounter = startDays; calCounter <= endDays; calCounter++) {
@@ -189,8 +235,8 @@ public class YakShopController {
 
 					evendata.put("title", "Milk -" + info.getMilkStock()
 							+ " - Skin - " + info.getWoolStock());
-					evendata.put("start",
-							(fmt.print(new DateTime(new Date()).plusDays(calCounter))));
+					evendata.put("start", (fmt.print(new DateTime(new Date())
+							.plusDays(calCounter))));
 					events.put(evendata);
 				}
 			}
@@ -198,7 +244,8 @@ public class YakShopController {
 			System.out.println(" end " + endDate.getTime());
 
 			response.setStatus(HttpServletResponse.SC_OK);
-			response.setContentType("");
+			response.setContentType("application/json");
+			
 			response.getOutputStream().write(events.toString().getBytes());
 			response.getOutputStream().flush();
 		} catch (IOException e) {
@@ -215,26 +262,7 @@ public class YakShopController {
 			}
 
 		}
-		/*
-		 * HeardInfo info = heardService.getHeardDetailsForTheDay(day);
-		 * JSONArray yaks = new JSONArray(); try { JSONObject stock = new
-		 * JSONObject(); stock.put("milk", info.getMilkStock());
-		 * stock.put("skins", info.getWoolStock());
-		 * 
-		 * response.setStatus(HttpServletResponse.SC_OK);
-		 * response.setContentType("");
-		 * response.getOutputStream().write(stock.toString().getBytes());
-		 * response.getOutputStream().flush();
-		 * 
-		 * } catch (IOException e) {
-		 * response.setStatus(HttpServletResponse.SC_BAD_REQUEST); // TODO
-		 * Auto-generated catch block e.printStackTrace(); } finally { try {
-		 * response.getOutputStream().close(); } catch (IOException e) {
-		 * response.setStatus(HttpServletResponse.SC_BAD_REQUEST); // TODO
-		 * Auto-generated catch block e.printStackTrace(); }
-		 * 
-		 * }
-		 */
+
 		return "";
 	}
 }
